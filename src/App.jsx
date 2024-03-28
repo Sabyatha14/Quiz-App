8e99d469587cf485d08c1fe2301a07291c1960b1
av18s <bsabyatha14@gmail.com>
2024-03-28 11:44:10 +0530

    App changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header.jsx';
 import Quiz from './components/Quiz.jsx';
 function App() {
     return <>
+    <div>MY APP</div>
     <Header />
     <main>
     <Quiz />
